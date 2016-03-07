dc3d501cc7b3d7f28b29b99c944469af4969820a
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-03-07 20:51:06 +0100

    We don't need a OptionsPriceWizard

--- dca/tl_iso_attributes.php
+++ dca/tl_iso_attributes.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -27,6 +27,14 @@
  */
 
 /**
- * Table tl_iso_attributes 
+ * Table tl_iso_attributes
  */
-$GLOBALS['TL_DCA']['tl_iso_attributes']['fields']['options']['inputType'] = 'optionPriceWizard';
+array_insert($GLOBALS['TL_DCA']['tl_iso_attributes']['fields']['options']['eval']['columnFields'], 2, array
+(
+	'price' => array
+	(
+		'label'     => &$GLOBALS['TL_LANG']['tl_iso_attributes']['options']['price'],
+		'inputType' => 'text',
+		'eval'      => array('class' => 'tl_text_2'),
+	),
+));
